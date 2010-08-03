35cfa1ed46a8b567960ee7b3a655fe026fff65a1
Adam Ballai <adam@blackacid.org>
2010-08-03 14:39:24 -0700

    Fix message template for better gmail rendering

--- OpenVBX/views/emails/message.php
+++ OpenVBX/views/emails/message.php
@@ -1,26 +1,26 @@
 <?php 
 $message_type = $message->type == 'sms'? 'SMS' : 'Voicemail';
-$content = "$message_type from {$message->caller}\n\n";
+echo "$message_type from {$message->caller}\n\n";
 if(!empty($message->content_text))
 {
 	$label = $message->type == 'sms'? 'Message' : 'Transcription';
-	$content .= "$label:\n\t" . $message->content_text . "\n";
+	echo "$label:\n\n" . $message->content_text . "";
 }
 
+echo "\n\n";
 
 if($message->type == 'voice')
 {
-	$content .= "\n-----------------------------------\n";
-	$content .= "Voicemail recorded {$message->created}\n";
-	$content .= "Length of voicemail: {$message->size} seconds\n";
+	echo "-----------------------------------\n\n";
+	echo "Voicemail recorded {$message->created}\n\n";
+	echo "Length of voicemail: {$message->size} seconds\n\n";
 }
 
-$content .= "\n-----------------------------------\n";
-$content .= "Link to details: ". site_url('/messages/details/'.$message->id). "\n";
+echo "-----------------------------------\n\n";
+echo "Link to details: ". site_url('/messages/details/'.$message->id). "\n\n";
 if($message->type == 'voice')
 {
-	$content .= "Link to audio: {$message->content_url}";
+	echo "Link to audio: {$message->content_url}\n\n";
 }
 ?>
 
-<?php echo $content ?>
