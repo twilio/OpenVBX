ddd912b9aa2d65d1e6074d9d6d439949d4789452
Adam Ballai <adam@blackacid.org>
2010-08-03 15:09:38 -0700

    Bump to 0.82 Hotfix release for email templates

--- OpenVBX/views/emails/message_assigned.php
+++ OpenVBX/views/emails/message_assigned.php
@@ -4,22 +4,25 @@ $content = "$message_type from {$message->caller}\n\n";
 if(!empty($message->content_text))
 {
 	$label = $message->type == 'sms'? 'Message' : 'Transcription';
-	$content .= "$label:\n\t" . $message->content_text . "\n";
+	$content .= "$label:\n\n" . $message->content_text . "\n";
 }
 
+$content .= "\n\n";
 
 if($message->type == 'voice')
 {
-	$content .= "\n-----------------------------------\n";
-	$content .= "Voicemail recorded {$message->created}\n";
-	$content .= "Length of voicemail: {$message->size} seconds\n";
+	$content .= "\n-----------------------------------\n\n";
+	$content .= "Voicemail recorded {$message->created}\n\n";
+	$content .= "Length of voicemail: {$message->size} seconds\n\n";
 }
 
-$content .= "\n-----------------------------------\n";
+$content .= "\n\n";
+
+$content .= "-----------------------------------\n\n";
 $content .= "Link to details: ". site_url('/messages/details/'.$message->id). "\n";
 if($message->type == 'voice')
 {
-	$content .= "Link to audio: {$message->content_url}";
+	$content .= "Link to audio: {$message->content_url}\n\n";
 }
 ?>
 
---
+++ sql-updates/39.sql
@@ -0,0 +1,2 @@
+UPDATE `settings` SET `value` = '0.82' WHERE `name` = 'version';
+UPDATE `settings` SET `value` = 39 WHERE `name` = 'schema-version';
