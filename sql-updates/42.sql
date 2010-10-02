e84ef8db4c8818fc4b9aea87c54f19866f30f0b7
Andrew Watson <andrewmoorewatson@gmail.com>
2010-10-01 23:23:40 -0400

    added OpenVBX version update

--- sql-updates/42.sql
+++ sql-updates/42.sql
@@ -1,3 +1,3 @@
 INSERT INTO settings (tenant_id,name,value) VALUES (1,'recording_host','');
 UPDATE `settings` SET `value` = 42 WHERE `name` = 'schema-version';
-
+UPDATE `settings` SET `value` = '0.85' WHERE `name` = 'version';
