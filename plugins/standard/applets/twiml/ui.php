85c4634257675b8b12a037777e3c6f2ec5026075
Adam Ballai <adam@blackacid.org>
2010-12-16 00:46:12 -0800

    Change references in client library, installation, and docs from 2008-08-01 to 2010-04-01

--- plugins/standard/applets/twiml/ui.php
+++ plugins/standard/applets/twiml/ui.php
@@ -1,7 +1,7 @@
 <div class="vbx-applet">
 
 		<h2>Build your own TwiML</h2>
-		<p><a href="http://www.twilio.com/docs/api/2008-08-01/twiml/" target="_blank">Learn more about TwiML</a></p>
+		<p><a href="http://www.twilio.com/docs/api/2010-04-01/twiml/" target="_blank">Learn more about TwiML</a></p>
 		<fieldset class="vbx-input-container">
 			<textarea name="twiml" class="large" placeholder="&lt;Say&gt;:)&lt;/Say&gt;"><?php echo AppletInstance::getValue('twiml') ?></textarea>
 		</fieldset>
