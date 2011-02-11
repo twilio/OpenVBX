8e17ec382a528d9688a246a699a04dc1480d80a1
minddog <adam@blackacid.org>
2011-02-10 16:49:34 -0800

    Add new invitiation based password reset and welcome mails for new users

---
+++ OpenVBX/views/emails/welcome-user.php
@@ -0,0 +1,6 @@
+Welcome to your new account, <?php echo $name ?>.
+
+
+To get setup accessing voicemail and SMS messages, you need to set a password for your account.
+	Click on the following link to complete the process:
+<?php echo $reset_url ?>
