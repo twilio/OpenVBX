13ba10361c5c733ff000916bb0a0663a1aedec9d
Rahim Sonawalla <rsonawalla@gmail.com>
2011-05-02 09:23:16 -0700

    Add DialCallStatus variable (bug fix)

--- OpenVBX/libraries/twilio.php
+++ OpenVBX/libraries/twilio.php
@@ -492,6 +492,7 @@
 		public $Duration;
 		public $RecordingDuration;
 		public $CallDuration;
+		public $DialCallStatus;
 		public $DialCallDuration;
 		public $RecordingUrl;
 		public $TranscriptionText;
@@ -508,7 +509,7 @@
 			$this->AuthToken = $token;
 			$this->AccountSid = $id;
 
-			foreach(array('CallSid', 'Caller', 'Called', 'CallStatus', 'DialStatus', 'Digits', 'Duration', 'RecordingUrl', 'TranscriptionText', 'SmsSid', 'To', 'SmsMessageSid', 'From', 'CallDuration', 'RecordingDuration', 'DialCallDuration') as $field)
+			foreach(array('CallSid', 'Caller', 'Called', 'CallStatus', 'DialStatus', 'Digits', 'Duration', 'RecordingUrl', 'TranscriptionText', 'SmsSid', 'To', 'SmsMessageSid', 'From', 'CallDuration', 'RecordingDuration', 'DialCallStatus', 'DialCallDuration') as $field)
 			{
 				$this->$field = (isset($_REQUEST[$field]) ? $_REQUEST[$field] : FALSE);
 			}
