f8540ee5c991b824a55c9fd3ac11f860f7bfa392
Shawn Parker <shawn@topfroggraphics.com>
2011-07-29 10:59:15 -0700

    Making the client-first-run text (hopefully) more helpful.

--- OpenVBX/views/client-first-run.php
+++ OpenVBX/views/client-first-run.php
@@ -1,6 +1,7 @@
 <div id="client-first-run">
 	<h3>OpenVBX now supports Twilio Client</h3>
-	<p>Use the status button to go online and make phone calls. To receive phone calls, use the Dial applet in a flow<br />
-	to designate yourself or your group as the recipient. <a href="#" class="dismiss">hide this message</a></p>
+	<p>You can make outbound calls from your browser at any time. Just use the Call button to dial out.</p> 
+	<p>To receive calls in your browser you must be the recipient of a Dial applet in a Flow through either direct selection or through a group.<br />You can then use the Status button to go online and receive phone calls.</p>
+	<p><a href="#" class="dismiss">hide this message</a></p>
 	<img class="arrow" src="<?php echo asset_url('/assets/i/client-first-run-arrow.png'); ?>" alt="" />
 </div>
