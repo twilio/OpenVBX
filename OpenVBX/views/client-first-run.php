5793e98b7cb0a66b81106a8f909c8bfac5eb5691
Shawn Parker <shawn@topfroggraphics.com>
2011-07-29 10:20:37 -0700

    Using asset url for image load in banner.

--- OpenVBX/views/client-first-run.php
+++ OpenVBX/views/client-first-run.php
@@ -1,7 +1,6 @@
 <div id="client-first-run">
 	<h3>OpenVBX now supports Twilio Client</h3>
-	<p>Use the status button to go online and 
-	make phone calls. To receive phone calls, use the Dial applet in a flow<br />
+	<p>Use the status button to go online and make phone calls. To receive phone calls, use the Dial applet in a flow<br />
 	to designate yourself or your group as the recipient. <a href="#" class="dismiss">hide this message</a></p>
-	<img class="arrow" src="../../assets/i/client-first-run-arrow.png" alt="" />
+	<img class="arrow" src="<?php echo asset_url('/assets/i/client-first-run-arrow.png'); ?>" alt="" />
 </div>
