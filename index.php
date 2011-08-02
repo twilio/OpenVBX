3172dd54261f5e9cf564d34e601ac5c9654b61b5
Shawn Parker <shawn@topfroggraphics.com>
2011-08-02 12:39:28 -0700

    re-adding default time-zone

--- index.php
+++ index.php
@@ -83,6 +83,7 @@ $system_folder = "system";
  |
 */
 $application_folder = dirname(__FILE__) . '/OpenVBX';
+date_default_timezone_set('UTC');
 
 /*
  |===============================================================
