bb8f584a976aa0abea24c53ce82141111e4f255e
Adam Anderly <adam@anderly.com>
2011-08-12 22:20:31 -0500

    Fix to properly use recording_host if specified.
    Signed-off-by: Shawn Parker <shawn@topfroggraphics.com>

--- OpenVBX/models/vbx_message.php
+++ OpenVBX/models/vbx_message.php
@@ -392,6 +392,14 @@ class VBX_Message extends Model {
 		$ci =& get_instance();
 		$ci->load->model('vbx_user');
 		$ci->load->model('vbx_group');
+		
+		$recording_host = $ci->settings->get('recording_host', VBX_PARENT_TENANT);
+		
+		$vm_url = $message->content_url;
+		if (trim($recording_host != '')) {
+			$vm_url = str_replace('api.twilio.com',$recording_host, $vm_url);
+		}
+		$message->content_url = $vm_url;
 
 		$users = array();
 		if($message->owner_type == 'user')
