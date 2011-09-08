278bf65358477706d073e10180818ae41c2d2509
Shawn Parker <shawn@topfroggraphics.com>
2011-09-08 16:13:16 -0700

    Fixing welcome message for Tenants
    When welcoming tenants we don't have a user name yet. So we'll just
    not address them by name.

--- OpenVBX/views/emails/welcome-user.php
+++ OpenVBX/views/emails/welcome-user.php
@@ -1,4 +1,4 @@
-Welcome to your new account, <?php echo $name ?>.
+Welcome to your new account<?php echo (!empty($name)) ? ', '.$name : ''; ?>.
 
 
 To get setup accessing voicemail and SMS messages, you need to set a password for your account.
