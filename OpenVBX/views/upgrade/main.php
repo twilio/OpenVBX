67a23320a68b887a26c7c5a9eabae52dbc23fefc
Shawn Parker <shawn@topfroggraphics.com>
2011-09-29 09:32:08 -0700

    Removing obsolete JS include statements

--- OpenVBX/views/upgrade/main.php
+++ OpenVBX/views/upgrade/main.php
@@ -4,10 +4,6 @@
 <head>
 	<title>Upgrade OpenVBX</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/assets/c/install.css" />
-	<!-- <script type="text/javascript" src="<?php echo base_url() ?>/assets/j/frameworks/jquery-1.6.2.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url() ?>/assets/j/plugins/jquery.validate.js"></script>
-	<?php $this->load->view('js-init'); ?>
-	<script type="text/javascript" src="<?php echo base_url() ?>/assets/j/upgrade.js"></script> -->
 
 	<?php $this->load->view('js-init'); ?>	
 	<?php if ($this->config->item('use_unminimized_js')): ?>
