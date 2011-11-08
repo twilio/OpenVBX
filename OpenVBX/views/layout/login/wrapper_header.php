25e9557b6347f82b4cd2cefc1351a978274e9f27
Shawn Parker <shawn@topfroggraphics.com>
2011-11-07 22:54:26 -0800

    Adding framebuster to logout
    Hopfully fixes a rare logout/timeout bug for tenants where a faulty
    redirect puts them on the parent's install instead of their own.

--- OpenVBX/views/layout/login/wrapper_header.php
+++ OpenVBX/views/layout/login/wrapper_header.php
@@ -1,3 +1,6 @@
+<script type="text/javascript">
+if (window != window.top) { window.top.location = window.location; }
+</script>
 <div class="<?php echo $layout ?>">
 
 <div id="wrapper" class="<?php echo $theme;?>-theme">
