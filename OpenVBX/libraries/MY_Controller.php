b9af6c40b981fc12e50b7f21fa2bdce45046c881
Shawn Parker <shawn@topfroggraphics.com>
2011-11-30 10:41:42 -0800

    Plugin store isn't truly deprecated...

--- OpenVBX/libraries/MY_Controller.php
+++ OpenVBX/libraries/MY_Controller.php
@@ -74,7 +74,7 @@ class MY_Controller extends Controller
 		$this->load->model('vbx_group');
 		$this->load->model('vbx_flow');
 		$this->load->model('vbx_flow_store');
-		// $this->load->model('vbx_plugin_store');
+		$this->load->model('vbx_plugin_store');
 		$this->load->helper('file');
 		$this->load->helper('twilio');
 		$this->load->library('session');
