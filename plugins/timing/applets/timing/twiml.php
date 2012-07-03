e0e100bb2cc7fb0de41d85ed18859dfc3812e723
Shawn Parker <shawn@twilio.com>
2012-07-03 16:20:25 -0700

    Fixing whitespace & comment style

--- plugins/timing/applets/timing/twiml.php
+++ plugins/timing/applets/timing/twiml.php
@@ -4,18 +4,19 @@ $response = new TwimlResponse;
 $now = date_create('now');
 $today = date_format($now, 'w') - 1;
 
-// The names of the applet instance variables for "from" and "to" times 
-// are of the form: "range_n_from" and "range_n_to" where "n" 
-// is a value between 0 and 6 (inclusive). 0 represents Monday
-// and 6 represents Sunday. In PHP, the value of date_format($now, 'w')
-// for Sunday is 0 - for Monday the value is 1 - and so on.
-// Here, we need to compensate for this by checking to see if the value
-// of date_format($now, 'w') - 1 is -1, and, if so, bring Sunday
-// back into the valid range of values by setting $today to 6.
-
-if ($today == -1) 
-{ 
-  $today = 6; 
+/** 
+ * The names of the applet instance variables for "from" and "to" times 
+ * are of the form: "range_n_from" and "range_n_to" where "n" 
+ * is a value between 0 and 6 (inclusive). 0 represents Monday
+ * and 6 represents Sunday. In PHP, the value of date_format($now, 'w')
+ * for Sunday is 0 - for Monday the value is 1 - and so on.
+ * Here, we need to compensate for this by checking to see if the value
+ * of date_format($now, 'w') - 1 is -1, and, if so, bring Sunday
+ * back into the valid range of values by setting $today to 6.
+ */
+if ($today == -1)
+{
+  $today = 6;
 }
 
 $response->redirect(AppletInstance::getDropZoneUrl(
