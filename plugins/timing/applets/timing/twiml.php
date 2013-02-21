f149602030378fff039602cce691a2f38147b368
D Keith Casey Jr <keith@caseysoftware.com>
2013-02-20 21:15:53 -0600

    Fix for timing not allowing open on Sundays. Interesting off by one error, 'w' is Sun-Sat:0-6, 'N' is Mon-Sun:1-7. Credit goes to tjlytle: https://github.com/tjlytle/OpenVBX/commit/e6b7d2156ad23339215661b85310270b893be051

--- plugins/timing/applets/timing/twiml.php
+++ plugins/timing/applets/timing/twiml.php
@@ -2,11 +2,11 @@
 $response = new TwimlResponse;
 
 $now = date_create('now');
-$today = date_format($now, 'w') - 1;
+$today = date_format($now, 'N') - 1;
 
-/** 
- * The names of the applet instance variables for "from" and "to" times 
- * are of the form: "range_n_from" and "range_n_to" where "n" 
+/**
+ * The names of the applet instance variables for "from" and "to" times
+ * are of the form: "range_n_from" and "range_n_to" where "n"
  * is a value between 0 and 6 (inclusive). 0 represents Monday
  * and 6 represents Sunday. In PHP, the value of date_format($now, 'w')
  * for Sunday is 0 - for Monday the value is 1 - and so on.
