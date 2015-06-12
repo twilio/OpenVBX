20d3ac716ea450ea67220147166120f730e7e11d
Shawn Parker <shawn@topfroggraphics.com>
2015-06-11 19:19:21 -0700

    Update SMS Applet to properly use `<Sms>` TwiML when sending messages during voice flows.

--- plugins/sms/applets/sms/twiml.php
+++ plugins/sms/applets/sms/twiml.php
@@ -3,7 +3,18 @@ $sms = AppletInstance::getValue('sms');
 $next = AppletInstance::getDropZoneUrl('next');
 
 $response = new TwimlResponse;
-$response->message($sms);
+
+// Call flows still use the legacy <Sms> TwiML
+// for sending messages during calls.
+if(AppletInstance::getFlowType() == 'voice')
+{
+	$response->sms($sms);
+}
+else
+{
+	$response->message($sms);
+}
+
 if(!empty($next))
 {
 	$response->redirect($next);
