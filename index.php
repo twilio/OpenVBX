bb6a01b4c9b65ab83650a08f951de1861465ccfc
Shawn Parker <shawn@topfroggraphics.com>
2015-08-22 15:55:29 -0700

    Turn of strick & deprecated errors
    Explicitly turn off strict and deprecated error messages in `index.php`. This
    should help with the way different versions of PHP handle error reporting.

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ function shutdown()
 | For more info visit:	http://www.php.net/error_reporting
 |
 */
-error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE ^ E_USER_WARNING);
+error_reporting(E_ALL & ~E_WARNING & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED & ~E_USER_WARNING);
 ini_set('display_errors', 'Off');
 ini_set('log_errors', 'On');
 
